644b8ea78f33241bc849b1d24e3e8b7aa2a970e3
reemsarhan <115352161+reemsarhan@users.noreply.github.com>
2023-08-18 23:33:00 +0300

    Create STD_TYPES.h

---
+++ LIB/STD_TYPES.h
@@ -0,0 +1,24 @@
+/*
+ * STD_TYPES.h
+
+ *
+ *  Created on: Aug 18, 2023
+ *      Author: hp
+ */
+
+#ifndef MCAL_STD_TYPES_H_
+#define MCAL_STD_TYPES_H_
+
+
+typedef unsigned char u8;
+typedef unsigned short int u16;
+typedef unsigned long int u32;
+
+typedef signed char s8;
+typedef signed short int s16;
+typedef signed long int s32;
+
+typedef float f32;
+typedef signed int f64;
+
+#endif /* MCAL_STD_TYPES_H_ */
