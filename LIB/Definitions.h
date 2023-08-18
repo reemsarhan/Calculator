1629c4eacffe02f8d9e64e1b8bfbfad1a4a394e8
reemsarhan <115352161+reemsarhan@users.noreply.github.com>
2023-08-18 23:33:39 +0300

    Create Definitions.h

---
+++ LIB/Definitions.h
@@ -0,0 +1,45 @@
+/*
+ * Definitions.h
+
+ *
+ *  Created on: Aug 18, 2023
+ *      Author: hp
+ */
+
+#ifndef LIB_DEFINITIONS_H_
+#define LIB_DEFINITIONS_H_
+
+
+#define PORTA 0
+#define PORTB 1
+#define PORTC 2
+#define PORTD 3
+
+#define INPUT 0
+#define OUTPUT 1
+
+#define PIN0 0
+#define PIN1 1
+#define PIN2 2
+#define PIN3 3
+#define PIN4 4
+#define PIN5 5
+#define PIN6 6
+#define PIN7 7
+
+#define HIGH 1
+#define LOW 0
+
+#define zero  0
+#define one   1
+#define two   2
+#define three 3
+#define four  4
+#define five  5
+#define six   6
+#define seven 7
+#define eight 8
+#define nine  9
+
+
+#endif /* LIB_DEFINITIONS_H_ */
